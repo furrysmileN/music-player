a602ff920916ffc753373f00810b1414d27a0061
Nemo <112535500+furrysmileN@users.noreply.github.com>
2022-10-15 15:28:57 +0800

    Update main.cpp
    Optimized the code and added notes

--- main.cpp
+++ main.cpp
@@ -4,16 +4,19 @@
 int listentime;
 unsigned long stoptime;
 unsigned long starttime;
-unsigned long nowtime;
+unsigned long nowtime; // Set up basic parameters
+int notenumber;
 int menuChoice;
+int Note;
+int Song[];
+int Duration[];
 int SongOne[] = {
-    NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+    NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4}; //  Set up each song's notes pitches and durations
 int DurationOne[] = {
     1000 / 4, 1000 / 8, 1000 / 8, 1000 / 4, 1000 / 4, 1000 / 4, 1000 / 4, 1000 / 4};
 int SongTwo[] = {
     NOTE_D3, NOTE_D3, NOTE_E3, NOTE_F3, NOTE_F3, NOTE_E3, NOTE_D3, NOTE_C3, NOTE_B3, NOTE_B3,
     NOTE_C3, NOTE_D3, NOTE_D3, NOTE_C3, NOTE_C3};
-
 int DurationTwo[] = {
     1000 / 4, 1000 / 4, 1000 / 4, 1000 / 4, 1000 / 4, 1000 / 4, 1000 / 4, 1000 / 4, 1000 / 4, 1000 / 4,
     1000 / 4, 1000 / 4, 1000 / 4 + 1000 / 8, 1000 / 8, 1000 / 2};
@@ -27,157 +30,94 @@ int DurationFour[] = {
     1000 / 16, 1000 / 16, 1000 / 4, 1000 / 16, 1000 / 16, 1000 / 4};
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); // start the Serial monitor and initialize the serial port
   delay(500);
-}
-int StrToHex(char str[])
-{
-  return (int)strtol(str, 0, 16);
 }
 void loop()
 {
-  Serial.println("");
-  Serial.println("program started");
-  Serial.println("which song do you want to listen ? ");
-  while (Serial.available())
+  Serial.println("esp32 program started"); // print out a sentence to show the program is working
+  while (Serial.available())               ////  the serial received data and read it
   {
-    Serial.read();
+    Serial.read();                         
   }
-  while (Serial.available() == 0)
+  while (Serial.available() == 0)         //make sure there is no bytes in serial port
   {
   }
-  menuChoice = Serial.parseInt();
+  menuChoice = Serial.parseInt();         //input 'which song' from serial monitor 
   Serial.print("you shoose to listen song ");
   Serial.println(menuChoice);
-  Serial.println("How long do you want to listen? (/s)");
   while (Serial.available())
   {
     Serial.read();
   }
-  while (Serial.available() == 0)
+  while (Serial.available() == 0) // repeat stage above, make sure there is no bytes in serial port
   {
   }
-  listentime = Serial.parseInt();
+  listentime = Serial.parseInt(); // input 'how long' from serial monitor
   Serial.print("you want to listen ");
   Serial.print(listentime);
   Serial.println(" s");
   delay(1000);
-  while (Serial.available())
-  {
-    Serial.read();
-  }
   switch (menuChoice)
   {
   case 1:
-    starttime = millis();
-    stoptime = listentime * 1000 + starttime;
-    for (int thisNote = 0; thisNote < 8; thisNote++)
+    notenumber = 8;  // for each song, its original length and notes are different
+    for ( Note = 0; Note < 8; Note++) //set up song information for each song
     {
-      tone(BUZZZER_PIN, SongOne[thisNote], DurationOne[thisNote]);
-      for (nowtime = millis(); nowtime > stoptime;)
-      {
-        goto nemo;
-      }
-      int pauseBetweenNotes = DurationOne[thisNote] * 1.30;
-      delay(pauseBetweenNotes);
-      for (; thisNote == 8;)
-      {
-        thisNote = 0;
-      }
+      Song[Note] = SongOne[Note];
+      Duration[Note] = DurationOne[Note];
     }
-  // stop the tone playing:
-    noTone(BUZZZER_PIN);
     break;
-
   case 2:
-    // humidity sensor code goes here
-    Serial.println("You choose 2");
-    Serial.println("Then I will play song 2");
-    starttime = millis();
-    stoptime = listentime * 1000 + starttime;
-    for (int thisNote2 = 0; thisNote2 < 16; thisNote2++)
+    notenumber = 16;
+    for (Note = 0; Note < 16; Note++)
     {
-
-        for (nowtime = millis(); nowtime > stoptime;)
-        {
-          goto nemo;
-        }
-        // to calculate the note duration, take one second divided by the note type.
-        // e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-        tone(BUZZZER_PIN, SongTwo[thisNote2], DurationTwo[thisNote2]);
-        // to distinguish the notes, set a minimum time between them.
-        // the note's duration + 30% seems to work well:
-        int pauseBetweenNotes = DurationTwo[thisNote2] * 1.30;
-        delay(pauseBetweenNotes);
-        // stop the tone playing:
-        noTone(BUZZZER_PIN);
-        for (; thisNote2 == 8;)
-        {
-          thisNote2 = 0;
-        }
-      }
+      Song[Note] = SongTwo[Note];
+      Duration[Note] = DurationTwo[Note];
+    }
     break;
 
   case 3:
-    // pressure sensor code goes here
-    Serial.println("You choose 3");
-    Serial.println("Then I will play song 3");
-    starttime = millis();
-    stoptime = listentime * 1000 + starttime;
-    for (int thisNote3 = 0; thisNote3 < 6; thisNote3++)
+    notenumber = 6;
+    for (Note = 0; Note < 6; Note++)
     {
-      for (nowtime = millis(); nowtime > stoptime;)
-      {
-        goto nemo;
-      }
-      // to calculate the note duration, take one second divided by the note type.
-      // e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-      tone(BUZZZER_PIN, SongThree[thisNote3], DurationThree[thisNote3]);
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
-      int pauseBetweenNotes = DurationThree[thisNote3] * 1.30;
-      delay(pauseBetweenNotes);
-      for (; thisNote3 == 6;)
-      {
-        thisNote3 = 0;
-      }
-      // stop the tone playing:
-      noTone(BUZZZER_PIN);
+      Song[Note] = SongThree[Note];
+      Duration[Note] = DurationThree[Note];
     }
     break;
 
   case 4:
-    // humidity sensor code goes here
-    Serial.println("You choose 4");
-    Serial.println("Then I will play song 4");
-    starttime = millis();
-    stoptime = listentime * 1000 + starttime;
-    for (int thisNote4 = 0; thisNote4 < 6; thisNote4++)
+    notenumber = 6;
+    for (Note = 0; Note < 6; Note++)
     {
-      for (nowtime = millis(); nowtime > stoptime;)
-      {
-        goto nemo;
-      }
-      // to calculate the note duration, take one second divided by the note type.
-      // e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-      tone(BUZZZER_PIN, SongFour[thisNote4], DurationFour[thisNote4]);
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
-      int pauseBetweenNotes = DurationFour[thisNote4] * 1.30;
-      delay(pauseBetweenNotes);
-      // stop the tone playing:
-      noTone(BUZZZER_PIN);
-      for (; thisNote4 == 6;)
-      {
-        thisNote4 = 0;
-      }
+      Song[Note] = SongTwo[Note];
+      Duration[Note] = DurationThree[Note];
     }
     break;
 
   default:
-    Serial.println("Please choose a valid selection");
+    Serial.println("Please choose a valid selection"); //if input valid number, print out warning message
     break;
   }
+  Serial.print(" ");
+  starttime = millis();                               //use 'time' function to record the strating time and ending time
+  stoptime = listentime * 1000 + starttime;            
+  for (int thisNote = 0; thisNote < notenumber; thisNote++)
+  {
+    tone(BUZZZER_PIN, Song[thisNote], Duration[thisNote]);   // play each note one by one
+    for (nowtime = millis(); nowtime > stoptime;)            // if the time's up, stop playing
+    {
+      goto nemo;
+    }
+    int pauseBetweenNotes = DurationOne[thisNote] * 1.30; // pause between notes makes the song more rhythmical
+    delay(pauseBetweenNotes);
+    for (; thisNote == notenumber-1;)                     // if the song plays to the end but still have time, replay from the beginning
+    {
+      thisNote = 0;
+    }
+  }
+
+  noTone(BUZZZER_PIN);                                   // stop the tone playing
 nemo:
   noTone(BUZZZER_PIN);
 }
